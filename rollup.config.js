f403b5c2437abb5ad7d65ca781710ff6bb144f9c
dean <>
2021-04-13 14:18:20 +0800

    Upgrade to yarn 2 & husky 6

--- rollup.config.js
+++ rollup.config.js
@@ -6,22 +6,14 @@ import pkg from './package.json'
 const input = './src/index.ts'
 
 export default [].concat(
-  build(
-    input,
-    {
-      file: pkg.main,
-      format: 'cjs',
-    },
-    { external: ['auto-bind'] }
-  ),
-  build(
-    input,
-    {
-      file: pkg.module,
-      format: 'esm',
-    },
-    { external: ['auto-bind'] }
-  ),
+  build(input, {
+    file: pkg.main,
+    format: 'cjs',
+  }),
+  build(input, {
+    file: pkg.module,
+    format: 'esm',
+  }),
   build(
     input,
     {
